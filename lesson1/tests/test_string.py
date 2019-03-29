7228b306436b30cb4f85ae6492624c35c56e670a
Ksenia Yarysh <ksana32@gmail.com>
2019-03-29 00:34:21 -0700

    deleted stuff

--- lesson1/tests/test_string.py
+++
@@ -1,9 +0,0 @@
-"""String tests """
-
-
-def test_string1():
-    """Copies string and ensures that lengths of new and source string are equal """
-    string1 = 'Hello'
-    string2 = 'world'
-    string3 = string1 + ' ' + string2
-    assert len(string3) == len(string1) + len(string2) + 1
