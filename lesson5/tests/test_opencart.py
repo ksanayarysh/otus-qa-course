2debb2bd85edef3eb51d1766f4c26653278535d9
Ксения Ярыш <ksana32@gmail.com>
2019-04-09 22:20:48 +0500

    Revert "opencart"
    This reverts commit 7efaba52f198173fd4e2b6ef09358e39f885b0b4.

--- lesson5/tests/test_opencart.py
+++
@@ -1,7 +0,0 @@
-"""Тестирование сайта opencart"""
-
-
-def test_open_cart(driver, get_url):
-    """Открываем выбранный url выбранным драйвером"""
-    driver.get("http://" + get_url + "/opencart/")
-    assert driver.current_url == "http://" + get_url + "/opencart/"
