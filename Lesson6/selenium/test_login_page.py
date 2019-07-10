c5bdfd0cdcd67439d189ca05997d7cf263baafff
ksanayarysh <47036605+ksanayarysh@users.noreply.github.com>
2019-07-10 20:50:43 +0500

    Update test_login_page.py

--- Lesson6/selenium/test_login_page.py
+++ Lesson6/selenium/test_login_page.py
@@ -9,8 +9,8 @@ class TestLoginPage:
         pytest.param("admin", "11", False, id='wrong'),
         pytest.param("test1", "\\t", False, id='wrong'),
         pytest.param("  ", "123", False, id='wrong'),
-        pytest.param("john", "wee", False, id='wrong'),
-        pytest.param("admin", "admin", True, id='right'),
+        pytest.param("john", "wee", False, id='wrong')
+#         pytest.param("admin", "admin", True, id='right'),
     ])
     def test_login(self, driver, user, password, expected):
         assert ("Dashboard".lower() in driver.title.lower()) == expected
